c014efb58fd8ae520a92d9f7a98fcd240965265b
Luis Chiquin <luischiquin@hotmail.es>
2021-03-30 11:12:04 -0600

    Aprendiendo Ciclos

--- JS/app.js
+++
@@ -1,5 +0,0 @@
-console.log("Esto es una cadena");
-console.log(14.36);
-console.log(true);
-console.log([1,2,3]);
-console.log({ "username": 'ryan', "score": 70.4, "hours": 14, "proffesional": true })
--- JS/bucles.js
+++ JS/bucles.js
@@ -1,6 +1,18 @@
-let count = 50;
-while  (count<101)
+//while mientras se cumple una condicion
+
+let count = 5;
+while  (count<10)
 {
     console.log(count);
     count++;
 }
+
+//for ciclo aumentando mientras que
+
+let names = ['Luis', 'Darwin', 'Hamiltong','Mayita'];
+
+for (count=0; count<names.length; count++)
+{
+    console.log("Bienvenido " + names[count]);
+}
+
